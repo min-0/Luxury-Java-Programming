c73f70629f240360d142852201a06f371015898e
min-0 <dhe08133@naver.com>
2021-03-17 19:39:16 +0900

    Chapter3

--- _2장2번.java
+++ _2장2번.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class _2장2번 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("2자리 정수 입력(10~99)>>");
         int n = sc.nextInt();
 
         if (n / 10 == n % 10) {
-            System.out.print("10의 자리와 1의 자리가 같습니다");
+            System.out.print("Yes! 10의 자리와 1의 자리가 같습니다");
         } else {
             System.out.print("달라요");
         }
