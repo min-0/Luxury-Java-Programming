2b69bee449b266d0f8dd579a4d594fbd951f8891
min-0 <dhe08133@naver.com>
2021-03-26 23:41:53 +0900

    반복문 연습

---
+++ _3장1번.java
@@ -0,0 +1,15 @@
+package ch3_;
+
+public class _3Àå1¹ø {
+
+	public static void main(String[] args) {
+		int sum = 0, i = 0;
+		
+		while(i<100) {
+			sum += i;
+			i += 2;
+		}
+		System.out.println(sum);
+	}
+
+}
