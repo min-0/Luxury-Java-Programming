b73e98919dc27404c562d7b693e86dca9b95f2fb
min-0 <dhe08133@naver.com>
2021-09-21 20:19:19 +0900

    21.09.20

---
+++ 2021-2/Chapter9/ch9_1.java
@@ -0,0 +1,16 @@
+package Chapter9;
+
+import javax.swing.*;
+
+public class ch9_1 extends JFrame {
+	public ch9_1() {
+		setTitle("Let's study Java");
+		setSize(400, 200);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		ch9_1 frame = new ch9_1();
+	}
+
+}
